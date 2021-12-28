22ff357653a7f74493cc8678ea8cebe868b6896e
pepoghst <88512425+pepoghst@users.noreply.github.com>
2021-12-28 16:38:04 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    while True:
+        audio = r.listen(source)
+
+        print(r.recognize_google(audio, language='pt'))
+        
